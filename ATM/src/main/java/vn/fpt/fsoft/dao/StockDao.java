add6909cf07e924ee70d7257579151c062ddf192
phanxuanmanh <iamme.xuanmanh@gmail.com>
2015-05-27 14:17:04 +0700

    add StockDao

---
+++ ATM/src/main/java/vn/fpt/fsoft/dao/StockDao.java
@@ -0,0 +1,12 @@
+package vn.fpt.fsoft.dao;
+
+import java.util.List;
+
+import vn.fpt.fsoft.model.Money;
+
+public class StockDao {
+public static List<Money> getMoneyList(int atmID){
+	
+	return null;
+}
+}
