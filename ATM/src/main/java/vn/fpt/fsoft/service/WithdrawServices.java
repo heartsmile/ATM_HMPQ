078a853c1cf750ae315451ab7ef94b9b47a136e5
phanxuanmanh <iamme.xuanmanh@gmail.com>
2015-05-27 14:08:49 +0700

    Xuan manh write WithDraw calculate

--- ATM/src/main/java/vn/fpt/fsoft/service/WithdrawServices.java
+++ ATM/src/main/java/vn/fpt/fsoft/service/WithdrawServices.java
@@ -4,7 +4,11 @@
 package vn.fpt.fsoft.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import vn.fpt.fsoft.dao.StockDao;
 import vn.fpt.fsoft.model.Money;
 
 /**
@@ -12,8 +16,43 @@ import vn.fpt.fsoft.model.Money;
  *
  */
 public class WithdrawServices {
-	
-	public ArrayList<Money> dispenseCash(float amount){
-		return null;
+	public List<Money> dispenseCash(int amount) {
+		List<Money> output = new ArrayList<Money>();
+		List<Money> stockList = StockDao.getMoneyList(123);
+		Collections.sort(stockList, new MoneyComaprator());
+		int res = amount;
+		for (Money m : stockList) {
+			if (res != 0) {
+				int tmpRest = (res % m.getValue());
+				int sheets = (res - tmpRest) / m.getValue();
+				if (m.getQuantity() >= sheets) {
+					res = tmpRest;
+					output.add(new Money(sheets, m.getValue()));
+				}
+			} else {
+				break;
+			}
+		}
+
+		return output;
+
 	}
+
+}
+
+class MoneyComaprator implements Comparator<Money> {
+
+	@Override
+	public int compare(Money m1, Money m2) {
+		if (m1.getValue() > m2.getValue()) {
+			return 1;
+		} else {
+			if (m1.getValue() < m2.getValue()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
+
 }
